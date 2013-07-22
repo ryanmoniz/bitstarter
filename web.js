719f2491e0a72a77f1578c32859fa5d9caf969fb
parallels <parallels@parallels-Parallels-Virtual-Platform.(none)>
2013-07-22 13:25:32 -0600

    modified web.js to port 5001

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var contents = fs.readFileSync('index.html','utf8');
+    var buffer = new Buffer(contents,'utf8');
+    response.send(buffer.toString());
+
+  //response.send('Hello World 2!');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5001;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
